690bd55c67facfd886307315981a9b2d471ba49e
Eileen <eileenburdekin@proton.me>
2024-01-10 23:53:13 -0800

    update footer centering

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,14 +2,16 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 
 export default function Footer() {
   const navStyle = {
-    padding: "10px",
-    position: "absolute",
-    bottom: 0,
-    width: "75%",
+    paddingTop: "150px",
+    // position: "absolute",
+    // bottom: 0,
+    width: "100%",
+    textAlign: "center",
   };
 
   const iconStyle = {
@@ -17,33 +19,27 @@ export default function Footer() {
     margin: "5px",
   };
 
-  // const copyrightStyle = {
-  //   // fontSize: "14px",
-  //   // fontWeight: "medium",
-  // };
-
   return (
-    <>
-      <div style={navStyle}>
-        <a
-          href="mailto:hello@eileenburdekin.com"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <FontAwesomeIcon icon={faEnvelope} size="2xl" style={iconStyle} />
-        </a>
-        <a href="https://github.com/eburdekin" target="_blank" rel="noreferrer">
-          <FontAwesomeIcon icon={faGithub} size="2xl" style={iconStyle} />
-        </a>
-        <a
-          href="https://www.linkedin.com/in/eburdekin/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <FontAwesomeIcon icon={faLinkedin} size="2xl" style={iconStyle} />
-        </a>
-        <Typography sx={{ fontSize: 14 }}>© Eileen Burdekin 2023</Typography>
-      </div>
-    </>
+    <Container maxWidth="md" sx={navStyle}>
+      <a
+        href="mailto:hello@eileenburdekin.com"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <FontAwesomeIcon icon={faEnvelope} size="2xl" style={iconStyle} />
+      </a>
+      <a href="https://github.com/eburdekin" target="_blank" rel="noreferrer">
+        <FontAwesomeIcon icon={faGithub} size="2xl" style={iconStyle} />
+      </a>
+      <a
+        href="https://www.linkedin.com/in/eburdekin/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <FontAwesomeIcon icon={faLinkedin} size="2xl" style={iconStyle} />
+      </a>
+      <Typography sx={{ fontSize: 14 }}>© Eileen Burdekin 2023</Typography>
+      {/* </div> */}
+    </Container>
   );
 }
