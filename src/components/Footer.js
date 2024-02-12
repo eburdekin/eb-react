05af3ddd8759c8d677d56a143d62bdcd36c9644c
Eileen <eileenburdekin@proton.me>
2024-02-11 21:58:00 -0800

    add contact icons to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,10 @@
+import ContactIcons from "./ContactIcons";
+
 export default function Footer() {
   return (
     // <div className="fixed bottom-0 left-1/2 transform -translate-x-1/2 w-full text-center">
     <div className="w-full text-center mt-10">
+      <ContactIcons />
       <p className="text-sm">
         &copy; {new Date().getFullYear().toString()} Eileen Burdekin
       </p>
