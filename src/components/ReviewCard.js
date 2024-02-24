29e50a84513550c3ceb89ec5658636be1669622b
Eileen <eileenburdekin@proton.me>
2024-02-23 22:00:10 -0800

    review card styling

--- src/components/ReviewCard.js
+++ src/components/ReviewCard.js
@@ -1,7 +1,7 @@
 const ReviewCard = (props) => {
   return (
-    <div className="text-center border mb-10 max-w-xs bg-gray-200 rounded-2xl">
-      <div className="w-full h-36 bg-gray-300 font-white border-b border-black flex items-center justify-around neutral rounded-t-2xl">
+    <div className="text-center border mb-10 max-w-xs bg-white rounded-2xl">
+      <div className="w-full h-36 bg-gray-200 flex items-center justify-around neutral rounded-t-2xl">
         <img src={props.image} className="h-20 w-20 rounded-full" alt="" />
         <div className="text-left w-44">
           <p className="font-bold text-md">{props.personName}</p>
@@ -13,7 +13,7 @@ const ReviewCard = (props) => {
       </div>
       <div className="justify-center my-6">
         <a href={props.goTo} target="_blank" rel="noreferrer">
-          <button className="mt-5 rounded-md text-sm bg-black px-4 py-2 text-white">
+          <button className="rounded-md text-sm bg-black px-4 py-2 text-white">
             Read it on LinkedIn
           </button>
         </a>
