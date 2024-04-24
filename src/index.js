a4e8ddde88d182d240b6c658b79d12b500d39587
Eileen Burdekin <eburdekin@proton.me>
2024-04-23 19:56:57 -0700

    remove react-router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { router } from "./router";
-import { RouterProvider } from "react-router-dom";
+import Home from "./main.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<RouterProvider router={router} />);
+root.render(<Home />);
--- src/router.js
+++
@@ -1,4 +0,0 @@
-import { createBrowserRouter } from "react-router-dom";
-import Home from "./pages/Home";
-
-export const router = createBrowserRouter([{ path: "/", element: <Home /> }]);
