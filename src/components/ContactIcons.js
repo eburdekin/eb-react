830cd6a041d5ea72225ee8b18b4b1029dd0c2229
Eileen Burdekin <eburdekin@proton.me>
2024-05-03 15:44:06 -0700

    refactor contact icons

--- src/components/ContactIcons.js
+++ src/components/ContactIcons.js
@@ -4,22 +4,30 @@ import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 export default function ContactIcons() {
-  const iconStyle = "text-white m-2 hover-effect";
+  const links = [
+    { to: "email", href: "mailto:eburdekin@proton.me", icon: faEnvelope },
+    { to: "github", href: "https://github.com/eburdekin", icon: faGithub },
+    {
+      to: "linkedin",
+      href: "https://www.linkedin.com/in/eburdekin/",
+      icon: faLinkedin,
+    },
+  ];
+
   return (
     <div className="items-center">
-      <a href="mailto:eburdekin@proton.me" target="_blank" rel="noreferrer">
-        <FontAwesomeIcon icon={faEnvelope} size="2x" className={iconStyle} />
-      </a>
-      <a href="https://github.com/eburdekin" target="_blank" rel="noreferrer">
-        <FontAwesomeIcon icon={faGithub} size="2x" className={iconStyle} />
-      </a>
-      <a
-        href="https://www.linkedin.com/in/eburdekin/"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <FontAwesomeIcon icon={faLinkedin} size="2x" className={iconStyle} />
-      </a>
+      {links.map((link) => {
+        return (
+          <a href={link.href} target="_blank" rel="noreferrer">
+            <FontAwesomeIcon
+              key={link.to}
+              icon={link.icon}
+              size="2x"
+              className="text-white m-2 hover-effect"
+            />
+          </a>
+        );
+      })}
     </div>
   );
 }
