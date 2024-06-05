f822509147295a4de568a4dcc38bbd12f15ef562
Eileen Burdekin <eburdekin@proton.me>
2024-06-04 20:23:34 -0700

    update skill card border on hover

--- src/components/SkillCards.js
+++ src/components/SkillCards.js
@@ -9,7 +9,7 @@ const SkillCards = ({ items }) => {
             key={index}
             className="relative group block p-1 sm:p-2 h-full w-full"
           >
-            <div className="skill-hover text-white rounded-md w-full p-1 sm:p-2 overflow-hidden group-hover:ring-1 ring-gray-200 relative z-20 transition-all duration-500">
+            <div className="skill-hover text-white rounded-md w-full p-1 sm:p-2 overflow-hidden group-hover:ring-1 ring-gray-600 relative z-20 transition-all duration-500">
               <div className="py-2 z-50 relative space-y-4">
                 <Icon className="w-6 sm:w-8 h-6 sm:h-8 mx-auto" />
                 <p className="text-md lg:text-lg text-center text-gray-300">
