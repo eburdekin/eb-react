ccd2942b3095fa9c1856bf57de7a224ca0c44824
Eileen Burdekin <eileenburdekin@proton.me>
2024-08-20 02:43:00 -0700

    add initial tests for contact form, update packages to get rid of warnings

--- src/tests/contact.test.js
+++ src/tests/contact.test.js
@@ -1,10 +1,6 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-
 import React from "react";
-import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import { act } from "react";
 import "@testing-library/jest-dom";
 import userEvent from "@testing-library/user-event";
 import Contact from "../sections/Contact";
@@ -54,20 +50,22 @@ describe("Contact form", () => {
     expect(sendButton).toBeDisabled();
   });
 
-  // test("Adding all inputs enables button", () => {
-  //   render(<Contact />);
+  test("Adding all inputs enables button", async () => {
+    render(<Contact />);
 
-  //   const nameInput = screen.getByLabelText(/your name/i);
-  //   const emailInput = screen.getByLabelText(/your email/i);
-  //   const messageInput = screen.getByLabelText(/your message/i);
+    const nameInput = screen.getByLabelText(/your name/i);
+    const emailInput = screen.getByLabelText(/your email/i);
+    const messageInput = screen.getByLabelText(/your message/i);
 
-  //   userEvent.type(nameInput, "Test Name");
-  //   userEvent.type(emailInput, "test@example.com");
-  //   userEvent.type(messageInput, "Test message");
+    await act(async () => {
+      userEvent.type(nameInput, "Test Name");
+      userEvent.type(emailInput, "test@example.com");
+      userEvent.type(messageInput, "Test message");
+    });
 
-  //   const sendButton = screen.getByRole("button", { name: /send/i });
-  //   expect(sendButton).toBeEnabled();
-  // });
+    const sendButton = screen.getByRole("button", { name: /send/i });
+    expect(sendButton).toBeEnabled();
+  });
 
   // test("Send button submits form and shows success message", async () => {
   //   const mockOnSubmit = jest.fn();
@@ -85,17 +83,5 @@ describe("Contact form", () => {
   //   userEvent.click(sendButton);
 
   //   await waitFor(() => expect(mockOnSubmit).toHaveBeenCalledTimes(1));
-
-  // Optionally check the arguments passed to onSubmit
-  // expect(mockOnSubmit).toHaveBeenCalledWith({
-  //   name: "Test Name", // Replace with expected values
-  //   email: "test@example.com",
-  //   message: "Test message",
-  // });
-
-  // const successMessage = await screen.findByText(
-  //   /form submitted successfully!/i
-  // );
-  // expect(successMessage).toBeInTheDocument();
   // });
 });
