59d50a6e2c150eb36652d52dd9cf2b2eec5d5188
Eileen Burdekin <eileenburdekin@proton.me>
2025-01-27 22:16:10 -0800

    remove credits overlay for now

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,14 +9,15 @@ const Footer = () => {
       </div>
 
       <span className="small-text">
-        &copy; {new Date().getFullYear().toString()} Eileen Burdekin •{" "}
+        &copy; {new Date().getFullYear().toString()} Eileen Burdekin
+        {/* •{" "}
         <span className="hover-text credits">
           Credits{" "}
           <div className="credits-popup">
             Built with React and deployed with Netlify. Text is set in Acorn
             Semibold and GT Planar Regular.
           </div>
-        </span>
+        </span> */}
       </span>
     </footer>
   );
